7672383190b52ce82f80b048791d8d3706b8544d
Sajid S <sajid@duck.com>
2024-06-02 12:51:13 +0530

    Update entrypoint.sh

--- Workflow_Actions/entrypoint.sh
+++ Workflow_Actions/entrypoint.sh
@@ -2,13 +2,13 @@
 
 echo "#=============----------------=============#"
 
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${INPUT_ACTOR}"
+git config --global user.name "${INPUT_COMMITTER_NAME}"
+git config --global user.email "${INPUT_COMMITTER_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
-git push --setupstream origin main
+git push --set-upstream origin main
 
 echo "#=============----------------=============#"
